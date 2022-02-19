c9fce83981a49c2cba574c337d7c9168220dbb57
Creedmastr <63053600+Creedmastr@users.noreply.github.com>
2022-02-19 09:38:01 +0100

    Version 1.1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from asyncore import write
+from cgitb import text
 import random
 import time
 
@@ -6,19 +7,44 @@ import time
 lower = 'abcdefghijklmnopqrstuvwxyz'
 upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '0123456789'
+symbols = "@_%*$"
 
 string = lower + upper + numbers
 lenght = int(input("Lenght of the password : "))
-password = ''.join(random.choice(string) for _ in range(lenght))
+if lenght < 3:
+    print("Please enter a lenght of 3 or plus")
+    time.sleep(10)
+    exit("Please enter a lenght of 3 or plus")
+elif lenght > 999:
+    print("Please enter a lenght of 999 or under 999")
+    time.sleep(10)
+    exit()
+else: 
+    pass
 
-writetext = input("Want you to write it in a text file ? (y/n) ")
-file = open("password.txt", "w+")
-
-if writetext == "y" or "yes" or "Y" or "YES":
-    file.write(password)
-    file.write(password)
+symbols_wanted = input("Do you want symbols in your password ? (y/n)")
+if symbols_wanted == "y":
+    string = lower + upper + numbers + symbols
+elif symbols_wanted == "n":
+    string = string
     pass
 else:
+    print("Please enter a valid value")
+    exit()
+
+password = ''.join(random.choice(string) for _ in range(lenght))
+
+if lower and upper and numbers in password: 
+    pass
+else:
+    password = ''.join(random.choice(string) for _ in range(lenght))
+
+textfile = input("Want you to write it in a text file ? (y/n) ")
+
+if textfile == "y":
+    file = open("password.txt", "w+")
+    file.write(password)
+    file.write(password)
+elif textfile == 'n':
     print("Your new password is : " + password)
     time.sleep(1000)
-
